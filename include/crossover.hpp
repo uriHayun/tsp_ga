e8ad8e8bca8bca21d8c58d236580577589b122d5
urih <urih20122222@gmail.com>
2026-07-15 03:52:29 +0300

    Find the AB-cycles from the AB-graph

--- include/crossover.hpp
+++ include/crossover.hpp
@@ -32,22 +32,34 @@ class EAX {
             Parent parent;
         };
 
+        // Hash function for using a tagged-edge-key in an unordered_set
+        struct TaggedEdgeHash {
+            std::size_t operator()(const TaggedEdge &te) const noexcept;
+        };
+
+        // Equality function for using a TaggedEdge in an unordered_set
+        struct TaggedEdgeEqual {
+            bool operator()(const TaggedEdge &lhs, const TaggedEdge &rhs) const noexcept;
+        };
+
         using EdgeKey = std::pair<int, int>;
 
-        struct EdgeKeyHash {
+        // Hash function for using an edge-key in an unordered_set
+        struct EdgeHash {
             std::size_t operator()(const EdgeKey &key) const noexcept;
         };
 
-        using EdgeSet = std::unordered_set<EdgeKey, EdgeKeyHash>;
+        using EdgeSet = std::unordered_set<EdgeKey, EdgeHash>;
+        using TaggedEdgeSet = std::unordered_set<TaggedEdge, TaggedEdgeHash, TaggedEdgeEqual>;
         using TaggedEdges = std::vector<TaggedEdge>;
         using ABGraph = std::vector<std::vector<TaggedEdge>>;
-
+        using ABCycle = std::vector<TaggedEdge>;
 
         // Extracts all edges from a tour
         static std::vector<Edge> getEdges(const Tour &tour);
 
         // Returns the canonical representation of an edge
-        static EdgeKey getEdgeKey(const Edge &edge);
+        static EdgeKey normalizeEdge(const Edge &edge);
 
         // Checks whether an edge exists in an edge-set
         static bool containsEdge(const EdgeSet &edgeSet, const Edge &edge);
@@ -66,7 +78,21 @@ class EAX {
             const std::vector<Edge> &edgesB);
         
         // Tags each edge with its originating parent
-        static ABGraph buildAdjacencyGraph(
+        static ABGraph buildAdjGraph(
             const std::vector<TaggedEdge> &edges,
-            int numCities);     
+            int numCities);
+
+        // Given endpoint of an edge, returns other endpoint
+        static int getOtherEndpoint(const TaggedEdge &te, int currCity);
+
+        // Builds the initial collection of unused edges,
+        // each edge being tracked once rather than once per each endpoint
+        static TaggedEdgeSet buildUnusedEdgesSet(const std::vector<TaggedEdge> &edges);
+
+        // Transforms the AB-graph into AB-cycles by repeatedly walking an alternating
+        // path between A/B edges from an arbitrary edge until returning to the starting
+        // city
+        static std::vector<ABCycle> getABCycles(
+            const ABGraph &graph, 
+            const TaggedEdges &edges);
 };
